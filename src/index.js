eed47bd44c0cdac7a4b4209c6067f5f75a876b54
Dominique Mattern <dominique.mattern@5minds.de>
2017-09-05 14:17:13 +0200

    :sparkles: Add exec promise function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+#!/usr/bin/env node
 'use strict';
+
+const exec = require('./exec-promise');
+
+const nodeModulePaths = exec('npm ls --parseable')
+  .then((stdout) => {
+    console.log(stdout);
+  });
