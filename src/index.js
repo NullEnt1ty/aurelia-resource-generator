f4d9cfed0e12deab2b423b6e939ded8e74fa2c6b
Dominique Mattern <dominique.mattern@5minds.de>
2017-09-05 14:45:43 +0200

    :construction: :sparkles: Use npm ls output

--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@
 'use strict';
 
 const exec = require('./exec-promise');
+const endOfLine = require('os').EOL;
+
+function parseNpmResult(npmResult) {
+  const nodeModulePaths = npmResult
+    .split(endOfLine)
+    .filter((nodeModulePath, index) => {
+      // first entry is always equal to current working directory path
+      if (index === 0) {
+        return false;
+      }
+
+      if (nodeModulePath === '') {
+        return false;
+      }
+
+      return true;
+    });
+
+  return nodeModulePaths;
+}
 
 const nodeModulePaths = exec('npm ls --parseable')
-  .then((stdout) => {
-    console.log(stdout);
+  .then((npmResult) => {
+    const nodeModules = parseNpmResult(npmResult);
+
+    if (nodeModules.length === 0) {
+      throw new Error('Could not find node_modules in current working directory');
+    }
+  })
+  .catch((error) => {
+    console.error(error.message);
+    process.exit(1);
   });
